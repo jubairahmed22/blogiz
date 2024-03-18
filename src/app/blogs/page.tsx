df94c6de76725d675bc04dd428a3c47586035eec
jubairahmed22 <jubairahmed060@gmail.com>
2024-03-18 14:51:20 +0600

    blog page dynamic page 3 detials page will be specials thats why it can not be come to server side use that functions generateStaticParams in blogId page

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -11,7 +11,7 @@ const BlogsPage = async() => {
          <h1 className="text-center text-4xl my-5">All blogs from <span className="text-accent">blogiz</span></h1>
          <p className="text-center text-xl text-gray-400 my-5 w-2/4 mx-auto">
         <i>
-        Ahrefs’ Meta Description Generator can be a valuable tool for website owners, digital marketers, and SEO professionals who want to optimize their website's meta descriptions for search engine visibility. By inputting relevant keywords and a brief description of the webpage's content, the tool can generate concise and compelling meta descriptions that accurately represent the page and attract clicks from search engine users. This use case helps improve the website's ranking and click-through rates on search engine result pages.
+        Ahrefs Meta Description Generator can be a valuable tool for website owners, digital marketers, and SEO professionals who want to optimize their website meta descriptions for search engine visibility. By inputting relevant keywords and a brief description of the webpages content, the tool can generate concise and compelling meta descriptions that accurately represent the page and attract clicks from search engine users. This use case helps improve the websites ranking and click-through rates on search engine result pages.
         </i>
         </p>
         <div className="grid grid-cols-3 gap-4 my-10">
